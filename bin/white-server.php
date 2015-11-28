7251f7a80ca9df99129771d9c1467fdd30f0a488
Weldon Sams <wjsams@gmail.com>
2015-11-27 23:28:03 -0800

    Standardizing style. Double over single quotes.

--- bin/white-server.php
+++ bin/white-server.php
@@ -1,11 +1,12 @@
 <?php
+
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use White\White;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
-require dirname(__DIR__) . '/bin/config.php';
+require dirname(__DIR__) . "/vendor/autoload.php";
+require dirname(__DIR__) . "/bin/config.php";
 
 $server = IoServer::factory(
     new HttpServer(
